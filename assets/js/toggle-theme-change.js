e1b7f7584a5efb44a712b9d08ef552602cf6dc36
Din Mohammad <din.aitvet@gmail.com>
2023-03-19 03:21:02 +0600

    js code  update

--- assets/js/toggle-theme-change.js
+++ assets/js/toggle-theme-change.js
@@ -5,28 +5,22 @@ window.onload = function() {
 function toggleMode() {
     let element = document.body;
     let mode = localStorage.getItem('mode');
-    console.log('akash',mode)
+    // console.log('akash',mode)
 
     if(mode == 'dark'){       
         localStorage.setItem("mode", "light");
-        element.classList.remove("dark");
-        element.classList.add("light");
         document.documentElement.setAttribute("data-theme", "light"); //set theme to light
-
-        console.log('light2')
-
+        // console.log('light2')
     }
     else if(mode == 'light'){ 
         
         localStorage.setItem("mode", "dark");
         element.classList.remove("light");
         element.classList.add("dark");
-        document.documentElement.setAttribute("data-theme", "dark")//set theme to light
-        console.log('dark2')
+        document.documentElement.setAttribute("data-theme", "dark")//set theme to dark
+        // console.log('dark2')
     }
-
-    console.log('toggle click done ')
-       
+    // console.log('toggle click done ')
 }
 
 function setMode() {
@@ -35,12 +29,10 @@ function setMode() {
     if(!mode){localStorage.setItem("mode", "light");}
 
     if(mode == 'light'){
-        document.body.classList.add('light');
-            document.documentElement.setAttribute("data-theme", "light"); //set theme to light
+        document.documentElement.setAttribute("data-theme", "light"); //set theme to light
 
     }else if(mode == 'dark'){
-        document.body.classList.add('dark');
-        document.documentElement.setAttribute("data-theme", "dark")//set theme to light
+        document.documentElement.setAttribute("data-theme", "dark")//set theme to dark
 
     }
 }
